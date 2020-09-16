39d49147e5568d17bb4f79b573d47a2bc5fc97d7
Anurag Pandey <anurag.bhumca18@gmail.com>
2020-09-16 22:30:26 +0530

    Added co-worker's section.

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
- let btn = document.getElementById("getTimezone");
- 
- 
- if(btn !== null){
- btn.addEventListener("click",Timezone);
- }
- 
- function Timezone(){
-   let time = document.getElementById("showTimezone");
-   time.innerHTML = Date();
- }
+let btn = document.getElementById('getTimezone');
+
+if (btn !== null) {
+  btn.addEventListener('click', Timezone);
+}
+
+function Timezone() {
+  let time = document.getElementById('showTimezone');
+  time.innerHTML = Date();
+}
+
+let btnCo = document.getElementById('getTimezoneCo');
+
+if (btnCo !== null) {
+  btnCo.addEventListener('click', TimezoneCo);
+}
+
+function TimezoneCo() {
+  const url = '';
+  // GET request using fetch()
+  fetch(url)
+    // Converting received data to JSON
+    .then((response) => response.json())
+    .then((json) => {
+      // Display result
+      document.getElementById('loc').innerHTML = json.timezone;
+      document.getElementById('dt').innerHTML = json.date_time_wti;
+    });
+}
